50d89914c4684f9e69248879ab45a41deb6eea1b
DevT007 <hustlerkush1450@gmail.com>
2026-01-25 02:40:23 +0530

    Add files via upload
    Q- You are given an array A of N integers. You perform this operation N-2 times: For each
    contiguous subarray of odd size greater than 2, you find the median of each subarray(Say
    medians obtained in a move are M1
    , M2
    , M3
    , …, Mk). In each move, you remove the first
    occurrence of value min(M1
    , M2
    , M3
    , …, Mk) from the original array. After removing the
    element the array size reduces by 1 and no void spaces are left. For example, if we remove
    element 2 from the array {1, 2, 3, 4}, the new array will be {1, 3, 4}. Print a single integer
    denoting the sum of numbers that are left in the array after performing the operations

---
+++ Assignment1/additional_questions/q3.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+int main() {
+
+    int n ;
+    cin >> n ;
+
+    int arr[1000] ;
+    for(int i = 0 ; i<n ; i++) {
+        cin >> arr[i] ;
+        cout << " " ;
+    }
+
+    int min = arr[0] ;
+    int max = arr[0] ;
+
+    for(int i = 1 ; i<n ; i++) {
+        if(arr[i] < min) {
+            min = arr[i] ;
+        }
+
+        if(arr[i] > max) {
+            max = arr[i] ;
+        }
+    }
+
+    cout << min + max << endl ;
+}
